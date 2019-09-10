30341ee5c244fb22bf4bf280ba496e0da7be71c0
Shubham kakkar <shubham.kakkar98@gmail.com>
2019-09-10 18:10:00 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,44 +1,8 @@
-import React from "react";
-import Segment from "./Segment";
-import { View, Text } from "react-native";
-
-function Panel({ title }) {
-  return (
-    <View style={{ flex: 1, backgroundColor: "orange" }}>
-      <Text style={{ color: "red" }}>
-        {title}
-      </Text>
-    </View>
-  )
-}
+import React from 'react';
+import Segment from './Segment';
 
 export default function App() {
-  return (
-    <>
-    <Segment
-      tiles={["tile A", "tile B", "tile C", "title D"]}
-      borderRadius={20}
-      segmentType={"default"}
-      inactiveTabTextStyle={{ color: "red" }}
-      activeTabTextStyle={{ color: "green" }}
-      activeTabHighlighterPanelColor={"teal"}
-    >
-      <Panel title="Shubham kakkar" />
-      <Panel title="title" />
-    </Segment>
-        <Segment
-        tiles={["tile A", "tile B", "tile C", "title D"]}
-        borderRadius={20}
-        segmentType={"default"}
-        inactiveTabTextStyle={{ color: "red" }}
-        activeTabTextStyle={{ color: "green" }}
-        activeTabHighlighterPanelColor={"teal"}
-      >
-        <Panel title="Shubham kakkar" />
-        <Panel title="title" />
-      </Segment>
-      </>
-  );
+  return <Segment  onPress={ (currentActiveTile) => {
+    console.log({currentActiveTile})
+  } } tiles={['tile A', 'tile B', 'tile B', 'tile D']} />;
 }
-
- // TODO: this.props.children[index] for segment
